bcaa87bdde494a15e01cc0834b202bc29d580f47
Ajay Sharma <gridking111@gmail.com>
2025-12-03 11:59:27 +0200

    Initial Commit

---
+++ EloApp/EloAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EloAppApp.swift
+//  EloApp
+//
+//  Created by Work Laptop on 03/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct EloAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EloAppTests/EloAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  EloAppTests.swift
+//  EloAppTests
+//
+//  Created by Work Laptop on 03/12/2025.
+//
+
+import Testing
+@testable import EloApp
+
+struct EloAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
